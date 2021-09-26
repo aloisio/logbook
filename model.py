649e1e0e136e335953a61db9e84d2deee4ce1296
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2021-09-26 12:43:14 -0400

    Data Model
    - Create core model entities

--- model.py
+++ model.py
@@ -1,6 +1,95 @@
+import datetime
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass, field, InitVar
 from pathlib import Path
+from typing import List, TypeVar
 
 
-class Logbook:
-    def __init__(self, root: Path):
-        pass
+@dataclass(frozen=True, order=True)
+class Error:
+    path: Path
+    message: str
+
+
+@dataclass(frozen=True)
+class ParseResult:
+    errors: List[Error] = field(default_factory=lambda: [])
+
+    @property
+    def valid(self) -> bool:
+        return not self.errors
+
+
+class Parsable(metaclass=ABCMeta):
+    def parse(self):
+        return self.Parser(self).parse()
+
+    @dataclass
+    class Parser(metaclass=ABCMeta):
+        context: 'ExtendsParsable'
+        result: ParseResult = ParseResult()
+
+        @abstractmethod
+        def parse(self) -> ParseResult:
+            pass
+
+
+ExtendsParsable = TypeVar('ExtendsParsable', bound=Parsable)
+
+
+@dataclass(frozen=True, order=True)
+class Logbook(Parsable):
+    root: Path
+
+    class Parser(Parsable.Parser):
+        def parse(self) -> ParseResult:
+            return self.result
+
+
+@dataclass(frozen=True, order=True)
+class Year(Parsable):
+    root: Path
+    year: int = field(init=False)
+    date: InitVar[datetime.date]
+
+    def __post_init__(self, date: datetime.date):
+        object.__setattr__(self, 'year', date.year)
+
+    class Parser(Parsable.Parser):
+        def parse(self) -> ParseResult:
+            return self.result
+
+
+@dataclass(frozen=True, order=True)
+class Month(Parsable):
+    root: Path
+    year: int = field(init=False)
+    month: int = field(init=False)
+    date: InitVar[datetime.date]
+
+    def __post_init__(self, date: datetime.date):
+        object.__setattr__(self, 'year', date.year)
+        object.__setattr__(self, 'month', date.month)
+
+    class Parser(Parsable.Parser):
+        def parse(self) -> ParseResult:
+            return self.result
+
+
+@dataclass(frozen=True, order=True)
+class Day(Parsable):
+    root: Path
+    year: int = field(init=False)
+    month: int = field(init=False)
+    day: int = field(init=False)
+    date: InitVar[datetime.date]
+
+    def __post_init__(self, date: datetime.date):
+        object.__setattr__(self, 'year', date.year)
+        object.__setattr__(self, 'month', date.month)
+        object.__setattr__(self, 'day', date.day)
+
+    class Parser(Parsable.Parser):
+        def parse(self) -> ParseResult:
+            print(self.context.day)
+            return self.result
--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,78 @@
-from model import Logbook
+import datetime
+import shutil
+from pathlib import Path
+
+from model import Logbook, Year, Month, Day
+
+TEST_ROOT = Path(__file__).parent
 
 
 class TestLogbook:
-    def test_valid_if_its_days_are_valid(self, tmp_path):
-        Logbook(tmp_path)
+    def test_dataclass(self, tmp_path):
+        logbook = Logbook(tmp_path)
+        assert logbook.root == tmp_path
+        assert logbook == Logbook(tmp_path)
+        assert logbook < Logbook(tmp_path / 'subdir')
+        assert logbook > Logbook(tmp_path.parent)
+
+    def test_parse_valid(self, tmp_path):
+        logbook_path = tmp_path / 'logbook'
+        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
+        result = Logbook(logbook_path).parse()
+        assert result.valid
+        assert not result.errors
+
+
+class TestYear:
+    def test_dataclass(self, tmp_path):
+        year1 = Year(tmp_path, datetime.date(2020, 1, 1))
+        year2 = Year(tmp_path, datetime.date(2019, 1, 1))
+        assert year1.root == year2.root == tmp_path
+        assert year1.year == 2020
+        assert year2.year == 2019
+        assert year2 < year1
+        assert year1 == Year(tmp_path, datetime.date(2020, 12, 31))
+
+    def test_parse_valid(self, tmp_path):
+        logbook_path = tmp_path / 'logbook'
+        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
+        result = Year(logbook_path, datetime.date(2021, 8, 19)).parse()
+        assert result.valid
+        assert not result.errors
+
+
+class TestMonth:
+    def test_dataclass(self, tmp_path):
+        month1 = Month(tmp_path, datetime.date(2021, 8, 20))
+        month2 = Month(tmp_path, datetime.date(2019, 12, 1))
+        assert month1.root == month2.root == tmp_path
+        assert month1.year == 2021
+        assert month1.month == 8
+        assert month2 < month1
+        assert month1 == Month(tmp_path, datetime.date(2021, 8, 31))
+
+    def test_parse_valid(self, tmp_path):
+        logbook_path = tmp_path / 'logbook'
+        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
+        result = Month(logbook_path, datetime.date(2021, 8, 19)).parse()
+        assert result.valid
+        assert not result.errors
+
+
+class TestDay:
+    def test_dataclass(self, tmp_path):
+        day1 = Day(tmp_path, datetime.date(2021, 8, 20))
+        day2 = Day(tmp_path, datetime.date(2019, 12, 1))
+        assert day1.root == day2.root == tmp_path
+        assert day1.year == 2021
+        assert day1.month == 8
+        assert day1.day == 20
+        assert day2 < day1
+        assert day1 == Day(tmp_path, datetime.date(2021, 8, 20))
+
+    def test_parse_valid(self, tmp_path):
+        logbook_path = tmp_path / 'logbook'
+        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
+        result = Day(logbook_path, datetime.date(2021, 8, 19)).parse()
+        assert result.valid
+        assert not result.errors
