5360b4f045e144d707b5c7d2af866ce281fd1f4e
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2021-09-28 00:27:23 -0400

    Data Model
    - Parse day and validate footer

--- model.py
+++ model.py
@@ -1,27 +1,41 @@
 import datetime
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field, InitVar
+from functools import cached_property, lru_cache
 from pathlib import Path
 from typing import List, TypeVar
 
+from lxml import html
+from lxml.html import HtmlElement
+from markdown import markdown
+
 
 @dataclass(frozen=True, order=True)
-class Error:
+class ParseError:
     path: Path
     message: str
 
 
 @dataclass(frozen=True)
 class ParseResult:
-    errors: List[Error] = field(default_factory=lambda: [])
+    errors: List[ParseError] = field(default_factory=lambda: [])
 
     @property
     def valid(self) -> bool:
         return not self.errors
 
+    def add_error(self, path: Path, message: str) -> 'ParseResult':
+        self.errors.append(ParseError(path, message))
+        return self
+
 
 class Parsable(metaclass=ABCMeta):
-    def parse(self):
+    @property
+    @abstractmethod
+    def path(self) -> Path:
+        pass
+
+    def parse(self) -> ParseResult:
         return self.Parser(self).parse()
 
     @dataclass
@@ -41,6 +55,14 @@ ExtendsParsable = TypeVar('ExtendsParsable', bound=Parsable)
 class Logbook(Parsable):
     root: Path
 
+    @cached_property
+    def path(self) -> Path:
+        return self.root
+
+    @cached_property
+    def years(self) -> List['Year']:
+        return [Year(self.root, datetime.date(2021, 1, 1))]
+
     class Parser(Parsable.Parser):
         def parse(self) -> ParseResult:
             return self.result
@@ -54,6 +76,19 @@ class Year(Parsable):
 
     def __post_init__(self, date: datetime.date):
         object.__setattr__(self, 'year', date.year)
+
+    @cached_property
+    def path(self) -> Path:
+        yyyy = f'{self.year:04d}'
+        return self.root / yyyy / f'{yyyy}.md'
+
+    @cached_property
+    def months(self) -> List['Month']:
+        return [Month(self.root, datetime.date(2021, 8, 1)), Month(self.root, datetime.date(2021, 9, 1))]
+
+    @cached_property
+    def days(self) -> List['Day']:
+        return [Day(self.root, datetime.date(2021, 8, 20)), Day(self.root, datetime.date(2021, 9, 19))]
 
     class Parser(Parsable.Parser):
         def parse(self) -> ParseResult:
@@ -70,6 +105,15 @@ class Month(Parsable):
     def __post_init__(self, date: datetime.date):
         object.__setattr__(self, 'year', date.year)
         object.__setattr__(self, 'month', date.month)
+
+    @cached_property
+    def path(self) -> Path:
+        yyyy, mm = f'{self.year:04d}', f'{self.month:02d}'
+        return self.root / yyyy / mm / f'{yyyy}{mm}.md'
+
+    @cached_property
+    def days(self) -> List['Day']:
+        return [Day(self.root, datetime.date(2021, 8, 20)), Day(self.root, datetime.date(2021, 9, 19))]
 
     class Parser(Parsable.Parser):
         def parse(self) -> ParseResult:
@@ -89,7 +133,26 @@ class Day(Parsable):
         object.__setattr__(self, 'month', date.month)
         object.__setattr__(self, 'day', date.day)
 
+    @cached_property
+    def path(self) -> Path:
+        yyyy, mm, dd = f'{self.year:04d}', f'{self.month:02d}', f'{self.day:02d}'
+        return self.root / yyyy / mm / dd / f'{yyyy}{mm}{dd}.md'
+
     class Parser(Parsable.Parser):
         def parse(self) -> ParseResult:
-            print(self.context.day)
+            if not self.context.path.exists():
+                self.result.add_error(self.context.path, 'Markdown file does not exist')
+                return self.result
+            tree = parse_markdown(self.context.path)
+            if len(tree) > 0 and tree[-1].tag != 'footer':
+                self.result.add_error(self.context.path, 'Missing footer')
             return self.result
+
+
+@lru_cache
+def parse_markdown(path: Path) -> HtmlElement:
+    return html.fragment_fromstring(
+        markdown(path.read_text(encoding='utf-8'),
+                 output_format='html',
+                 extensions=['markdown.extensions.extra']),
+        create_parent=True)
--- test/test_model.py
+++ test/test_model.py
@@ -1,8 +1,10 @@
 import datetime
+import re
 import shutil
 from pathlib import Path
+from typing import Callable
 
-from model import Logbook, Year, Month, Day
+from model import Logbook, Year, Month, Day, ParseError
 
 TEST_ROOT = Path(__file__).parent
 
@@ -15,12 +17,17 @@ class TestLogbook:
         assert logbook < Logbook(tmp_path / 'subdir')
         assert logbook > Logbook(tmp_path.parent)
 
+    def test_path(self, tmp_path):
+        logbook = Logbook(tmp_path)
+        assert logbook.path == tmp_path
+
     def test_parse_valid(self, tmp_path):
-        logbook_path = tmp_path / 'logbook'
-        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
-        result = Logbook(logbook_path).parse()
+        logbook_path = create_logbook_files(tmp_path)
+        logbook = Logbook(logbook_path)
+        result = logbook.parse()
         assert result.valid
         assert not result.errors
+        assert logbook.years == [Year(logbook_path, datetime.date(2021, 8, 20))]
 
 
 class TestYear:
@@ -33,12 +40,20 @@ class TestYear:
         assert year2 < year1
         assert year1 == Year(tmp_path, datetime.date(2020, 12, 31))
 
+    def test_path(self, tmp_path):
+        year = Year(tmp_path, datetime.date(2021, 1, 4))
+        assert year.path == tmp_path / '2021/2021.md'
+
     def test_parse_valid(self, tmp_path):
-        logbook_path = tmp_path / 'logbook'
-        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
-        result = Year(logbook_path, datetime.date(2021, 8, 19)).parse()
+        logbook_path = create_logbook_files(tmp_path)
+        year = Year(logbook_path, datetime.date(2021, 8, 20))
+        result = year.parse()
         assert result.valid
         assert not result.errors
+        assert year.months == [Month(logbook_path, datetime.date(2021, 8, 20)),
+                               Month(logbook_path, datetime.date(2021, 9, 19))]
+        assert year.days == [Day(logbook_path, datetime.date(2021, 8, 20)),
+                             Day(logbook_path, datetime.date(2021, 9, 19))]
 
 
 class TestMonth:
@@ -51,12 +66,18 @@ class TestMonth:
         assert month2 < month1
         assert month1 == Month(tmp_path, datetime.date(2021, 8, 31))
 
+    def test_path(self, tmp_path):
+        month = Month(tmp_path, datetime.date(2021, 1, 4))
+        assert month.path == tmp_path / '2021/01/202101.md'
+
     def test_parse_valid(self, tmp_path):
-        logbook_path = tmp_path / 'logbook'
-        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
-        result = Month(logbook_path, datetime.date(2021, 8, 19)).parse()
+        logbook_path = create_logbook_files(tmp_path)
+        month = Month(logbook_path, datetime.date(2021, 8, 20))
+        result = month.parse()
         assert result.valid
         assert not result.errors
+        assert month.days == [Day(logbook_path, datetime.date(2021, 8, 20)),
+                              Day(logbook_path, datetime.date(2021, 9, 19))]
 
 
 class TestDay:
@@ -70,9 +91,38 @@ class TestDay:
         assert day2 < day1
         assert day1 == Day(tmp_path, datetime.date(2021, 8, 20))
 
+    def test_path(self, tmp_path):
+        day = Day(tmp_path, datetime.date(2021, 1, 4))
+        assert day.path == tmp_path / '2021/01/04/20210104.md'
+
     def test_parse_valid(self, tmp_path):
-        logbook_path = tmp_path / 'logbook'
-        shutil.copytree(TEST_ROOT / 'resources', logbook_path)
-        result = Day(logbook_path, datetime.date(2021, 8, 19)).parse()
+        logbook_path = create_logbook_files(tmp_path)
+        day = Day(logbook_path, datetime.date(2021, 8, 20))
+        result = day.parse()
+        assert day.parsed
         assert result.valid
         assert not result.errors
+
+    def test_parse_invalid_file_does_not_exist(self, tmp_path):
+        logbook_path = create_logbook_files(tmp_path)
+        day = Day(logbook_path, datetime.date(2021, 8, 19))
+        result = day.parse()
+        assert day.parsed
+        assert ParseError(day.path, 'Markdown file does not exist') in result.errors
+
+    def test_parse_invalid_missing_footer(self, tmp_path):
+        def remove_footer(day_text):
+            return re.sub(r'<footer.*?footer>', '', day_text)
+
+        logbook_path = create_logbook_files(tmp_path, remove_footer)
+        day = Day(logbook_path, datetime.date(2021, 8, 20))
+        result = day.parse()
+        assert ParseError(day.path, 'Missing footer') in result.errors
+
+
+def create_logbook_files(root: Path, day_mutator: Callable[[str], str] = lambda s: s):
+    logbook_path = root / 'logbook'
+    shutil.copytree(TEST_ROOT / 'resources', logbook_path)
+    day_path = logbook_path / '2021' / '08' / '20' / '20210820.md'
+    day_path.write_text(day_mutator(day_path.read_text()))
+    return logbook_path
