70b319c322ff60960a3a916d0004ee0159d347d6
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2022-12-31 12:10:21 -0500

    Checksum command-line tool options

--- src/chksum.py
+++ src/chksum.py
@@ -1,6 +1,7 @@
 import hashlib
 import re
 import sys
+from argparse import ArgumentParser
 from itertools import filterfalse
 from pathlib import Path
 
@@ -10,12 +11,12 @@ CHKSUM_PATTERN = re.compile(r'^.*-(?P<checksum>[0-9a-fA-F]{4})$')
 def chksum(path: Path) -> str:
     blake2b = hashlib.blake2b(digest_size=2)
     with path.open('rb') as input_file:
-        while buffer := input_file.read(2 ** 20):
+        while buffer := input_file.read(2 ** 22):
             blake2b.update(buffer)
     return blake2b.hexdigest()
 
 
-def main(*patterns):
+def main(write: bool, *patterns:str):
     paths = list(map(Path, patterns))
     files = set(filter(Path.exists, filter(Path.is_file, paths)))
     glob_patterns = map(Path.as_posix, filterfalse(Path.exists, paths))
@@ -24,13 +25,13 @@ def main(*patterns):
     for path in sorted(set(files).union(set(glob_files))):
         checksum = chksum(path)
         match = CHKSUM_PATTERN.match(path.stem)
-        original_checksum = match.group('checksum')
+        original_checksum = match.group('checksum') if match else None
         if match and original_checksum == checksum:
             print(f'Checksum OK {path}:')
         if match and original_checksum != checksum:
             print(f'Checksum FAIL: {path} checksum is {checksum}')
             failures.append(path)
-        if not match:
+        if not match and write:
             new_path = path.parent / f'{path.stem}-{chksum(path)}{path.suffix}'
             path.rename(new_path)
             print(f'Checksum added: {new_path}')
@@ -38,4 +39,8 @@ def main(*patterns):
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    parser = ArgumentParser()
+    parser.add_argument('-w', '--write', action='store_true', help='append checksum to file name')
+    parser.add_argument('patterns', metavar='FILE', type=str, nargs='+', help='glob pattern for file(s) to check')
+    options = parser.parse_args()
+    sys.exit(main(options.write, *options.patterns))
