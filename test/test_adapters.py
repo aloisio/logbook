06dfda8a0a32b55c935899a242556deeb79a765e
tester <tester@example.com>
2023-02-28 19:23:12 -0500

    AudioFileMetadata
    - Fix unit tests

--- test/test_adapters.py
+++ test/test_adapters.py
@@ -14,11 +14,11 @@ def test_default_audio_adapter():
 
 def test_default_file_type_adapter_image():
     adapter: FileTypeAdapter = DefaultFileTypeAdapter()
-    assert adapter.is_image(Path('sierpinski.jpg'))
-    assert not adapter.is_image(Path('100Hz_44100Hz_16bit_05sec.mp3'))
+    assert adapter.is_image(Path(__file__).parent / 'sierpinski.jpg')
+    assert not adapter.is_image(Path(__file__).parent / '100Hz_44100Hz_16bit_05sec.mp3')
 
 
 def test_default_file_type_adapter_audio():
     adapter: FileTypeAdapter = DefaultFileTypeAdapter()
-    assert not adapter.is_audio(Path('sierpinski.jpg'))
-    assert adapter.is_audio(Path('100Hz_44100Hz_16bit_05sec.mp3'))
+    assert not adapter.is_audio(Path(__file__).parent / 'sierpinski.jpg')
+    assert adapter.is_audio(Path(__file__).parent / '100Hz_44100Hz_16bit_05sec.mp3')
